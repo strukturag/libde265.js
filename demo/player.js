b0ef96e490ad64e6cae11f51855a836960994319
Joachim Bauch <bauch@struktur.de>
2014-04-07 15:17:36 +0200

    Various API changes + optimizations. Added JavaScript object API for decoding / image display. Moved Demo JavaScript to custom file to allow loading from gh-pages. Process two pixels per iteration in YUV -> RGB conversion.

---
+++ demo/player.js
@@ -0,0 +1,89 @@
+require(['libde265'], function(libde265) {
+    
+    var playback = function(url) {
+        url = url || "paris-ra-wpp.bin";
+        var req = new XMLHttpRequest();
+        req.open("get", url, true);
+        req.responseType = "arraybuffer";
+        
+        var canvas = document.getElementById("video");
+        var ctx = canvas.getContext("2d");
+        var imageData = null;
+
+        var displayImage = function(img) {
+            var w = img.get_width();
+            var h = img.get_height();
+            if (w != canvas.width || h != canvas.height || !imageData) {
+                canvas.width = w;
+                canvas.height = h;
+                imageData = ctx.createImageData(w, h);
+                var imageDataData = imageData.data;
+                for (var i=0; i<w*h; i++) {
+                    imageDataData[i*4+3] = 255;
+                }
+            }
+            
+            img.display(imageData, function(displayImage, displayImageData) {
+                ctx.putImageData(imageData, 0, 0);
+            });
+        };
+
+        req.onload = function(event) {
+            // TODO(fancycode): move decoding to WebWorker
+            var decoder = new libde265.Decoder();
+            decoder.set_image_callback(function(image) {
+                displayImage(image);
+                image.free();
+            });
+            
+            var data = req.response;
+            var pos = 0;
+            var remaining = data.byteLength;
+            
+            var decode = function() {
+                var err;
+                if (remaining === 0) {
+                    err = decoder.flush();
+                } else {
+                    var l = 40960;
+                    if (l > remaining) {
+                        l = remaining;
+                    }
+                    var tmp = new Uint8Array(data, pos, l);
+                    err = decoder.push_data(tmp);
+                    pos += l;
+                    remaining -= l;
+                }
+                if (!libde265.de265_isOK(err)) {
+                    console.log("Pushed", err, libde265.de265_get_error_text(err));
+                    return;
+                }
+                
+                decoder.decode(function(err) {
+                    if (!libde265.de265_isOK(err)) {
+                        console.log("Decode", err, libde265.de265_get_error_text(err));
+                        return;
+                    }
+                    
+                    if (remaining > 0 || decoder.has_more()) {
+                        setTimeout(decode, 0);
+                        return;
+                    }
+                    
+                    decoder.free();
+                });
+            }
+            
+            setTimeout(decode, 0);
+        };
+        
+        req.send();
+    };
+    
+    var button = document.getElementById("play");
+    button.addEventListener("click", function() {
+        playback();
+        return false;
+    }, false); 
+    
+});
--- post.js
+++ post.js
@@ -103,6 +103,153 @@
         de265_get_parameter_bool: Module.cwrap('de265_get_parameter_bool', 'number', ['number', 'number'])
     };
 
+    var Image = function(img) {
+        this.img = img;
+        this.width = null;
+        this.height = null;
+    };
+
+    Image.prototype.free = function() {
+    };
+
+    Image.prototype.get_width = function() {
+        if (this.width === null) {
+            this.width = api.de265_get_image_width(this.img, 0);
+        }
+        return this.width;
+    };
+
+    Image.prototype.get_height = function() {
+        if (this.height === null) {
+            this.height = api.de265_get_image_height(this.img, 0);
+        }
+        return this.height;
+    };
+
+    Image.prototype.display = function(imageData, callback) {
+        // TODO(fancycode): move to WebWorker
+        var w = this.get_width();
+        var h = this.get_height();
+        var stride = Module._malloc(4);
+        var y = api.de265_get_image_plane(this.img, 0, stride);
+        var stridey = Module.getValue(stride, "i32");
+        var u = api.de265_get_image_plane(this.img, 1, stride);
+        var strideu = Module.getValue(stride, "i32");
+        var v = api.de265_get_image_plane(this.img, 2, stride);
+        var stridev = Module.getValue(stride, "i32");
+        Module._free(stride);
+        var yval;
+        var uval;
+        var vval;
+        var xpos = 0;
+        var ypos = 0;
+        var w2 = w >> 1;
+        var HEAPU8 = Module.HEAPU8;
+        var imageDataData = imageData.data;
+        var maxi = w2*h;
+        var yoffset = y;
+        var uoffset = u;
+        var voffset = v;
+        var x2;
+        var i2;
+        for (var i=0; i<maxi; i++) {
+            i2 = i << 1;
+            x2 = (xpos << 1);
+            yval = 1.164 * (HEAPU8[yoffset + x2] - 16);
+
+            uval = HEAPU8[uoffset + xpos] - 128;
+            vval = HEAPU8[voffset + xpos] - 128;
+            imageDataData[(i2<<2)+0] = yval + 1.596 * vval;
+            imageDataData[(i2<<2)+1] = yval - 0.813 * vval - 0.391 * uval;
+            imageDataData[(i2<<2)+2] = yval + 2.018 * uval;
+
+            yval = 1.164 * (HEAPU8[yoffset + x2 + 1] - 16);
+            imageDataData[((i2+1)<<2)+0] = yval + 1.596 * vval;
+            imageDataData[((i2+1)<<2)+1] = yval - 0.813 * vval - 0.391 * uval;
+            imageDataData[((i2+1)<<2)+2] = yval + 2.018 * uval;
+
+            xpos++;
+            if (xpos === w2) {
+                xpos = 0;
+                ypos++;
+                yoffset = y + (ypos * stridey);
+                uoffset = u + ((ypos >> 1) * strideu);
+                voffset = v + ((ypos >> 1) * stridev);
+            }
+        }
+        callback(this, imageData);
+    };
+
+    var Decoder = function() {
+        this.image_callback = null;
+        this.more = Module._malloc(2);
+        this.ctx = api.de265_new_decoder();
+        api.de265_set_parameter_bool(this.ctx, api.DE265_DECODER_PARAM_BOOL_SEI_CHECK_HASH, true);
+    };
+
+    Decoder.prototype.free = function() {
+        api.de265_free_decoder(this.ctx);
+        this.ctx = null;
+        Module._free(this.more);
+        this.more = null;
+    };
+
+    Decoder.prototype.set_image_callback = function(callback) {
+        this.image_callback = callback;
+    };
+
+    Decoder.prototype.reset = function() {
+        api.de265_reset(this.ctx);
+    };
+
+    Decoder.prototype.push_data = function(data, pts) {
+        pts = pts || 0;
+        return api.de265_push_data(this.ctx, data, data.length, pts, 0);
+    };
+
+    Decoder.prototype.flush = function() {
+        return api.de265_flush_data(this.ctx);
+    };
+
+    Decoder.prototype.has_more = function() {
+        return Module.getValue(this.more, "i16") !== 0;
+    };
+
+    Decoder.prototype.decode = function(callback) {
+        var err;
+        Module.setValue(this.more, "i16", 0);
+        while (true) {
+            err = api.de265_decode(this.ctx, this.more);
+            switch (err) {
+            case api.DE265_ERROR_WAITING_FOR_INPUT_DATA:
+                Module.setValue(this.more, "i16", 0);
+                err = api.DE265_OK;
+                break;
+
+            default:
+                if (!api.de265_isOK(err)) {
+                    Module.setValue(this.more, "i16", 0);
+                }
+            }
+
+            var img = api.de265_get_next_picture(this.ctx);
+            if (img) {
+                if (this.image_callback) {
+                    this.image_callback(new Image(img));
+                }
+                break;
+            }
+
+            if (Module.getValue(this.more, "i16") === 0) {
+                break;
+            }
+        }
+        callback(err);
+        return;
+    };
+
+    api.Decoder = Decoder;
+
     return api;
 
 });
